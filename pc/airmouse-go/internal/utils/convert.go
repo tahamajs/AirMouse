981941e9cabcf7082bb3691035f3743d0fce49d8
taha <tahamaj4@gmail.com>
2026-05-31 23:44:27 +0330

    feat(utils): add utility functions for conversion, file handling, networking, randomness, time formatting, and validation

---
+++ pc/airmouse-go/internal/utils/convert.go
@@ -0,0 +1,19 @@
+package utils
+
+import (
+	"strconv"
+)
+
+// AtoiSafe converts a string to int, returning a default value on error.
+func AtoiSafe(s string, defaultValue int) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		return defaultValue
+	}
+	return val
+}
+
+// Itoa is a wrapper for strconv.Itoa.
+func Itoa(i int) string {
+	return strconv.Itoa(i)
+}
