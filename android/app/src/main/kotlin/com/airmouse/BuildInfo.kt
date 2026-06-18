f55f7d664e73114511e9ae3e83aeb4720263645f
taha <tahamaj4@gmail.com>
2026-06-18 16:43:10 +0330

    feat(service): add PreferencesManager to GestureInferenceService
    Import the `PreferencesManager` utility into `GestureInferenceService` to enable access to user settings during gesture processing.

--- android/app/src/main/kotlin/com/airmouse/BuildInfo.kt
+++ android/app/src/main/kotlin/com/airmouse/BuildInfo.kt
@@ -5,6 +5,6 @@ object BuildInfo {
     const val APP_NAME = "app"
     const val VERSION_NAME = "3.0.0"
     const val VERSION_CODE = 100
-    const val BUILD_TIME = "2026-06-18 16:36:37"
+    const val BUILD_TIME = "2026-06-18 16:40:01"
     const val BUILD_TYPE = "build"
 }
