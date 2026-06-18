6babab50b03810ccec60b2bf6666c07d7889671c
taha <tahamaj4@gmail.com>
2026-06-18 16:38:25 +0330

    refactor(ui): update ThemesViewModel imports and update build timestamp
    - Add `PreferencesManager` import to `ThemesViewModel`.
    - Update `BUILD_TIME` in `BuildInfo.kt`.

--- android/app/src/main/java/com/airmouse/presentation/ui/themes/ThemesViewModel.kt
+++ android/app/src/main/java/com/airmouse/presentation/ui/themes/ThemesViewModel.kt
@@ -2,6 +2,7 @@ package com.airmouse.presentation.ui.themes
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.airmouse.utils.PreferencesManager
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
