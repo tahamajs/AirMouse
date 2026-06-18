7af3e4d79a85ef9533a84f901e8ecc058a995b63
taha <tahamaj4@gmail.com>
2026-06-19 00:21:19 +0330

    update android project configuration and dependencies, including build scripts, manifest, and build info generation.

--- android/app/src/main/kotlin/com/airmouse/BuildInfo.kt
+++ android/app/src/main/kotlin/com/airmouse/BuildInfo.kt
@@ -5,6 +5,6 @@ object BuildInfo {
     const val APP_NAME = "app"
     const val VERSION_NAME = "3.0.0"
     const val VERSION_CODE = 100
-    const val BUILD_TIME = "2026-06-18 16:51:03"
-    const val BUILD_TYPE = "build"
+    const val BUILD_TIME = "2026-06-18 17:33:08"
+    const val BUILD_TYPE = "assembleDebug"
 }
