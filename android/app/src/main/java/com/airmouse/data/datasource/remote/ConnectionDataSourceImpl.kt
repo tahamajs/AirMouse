1f2422b59acbc7e56eaa8fd7d49af97c240da523
taha <tahamaj4@gmail.com>
2026-06-19 18:02:21 +0330

    updating files

--- android/app/src/main/java/com/airmouse/data/datasource/remote/ConnectionDataSourceImpl.kt
+++ android/app/src/main/java/com/airmouse/data/datasource/remote/ConnectionDataSourceImpl.kt
@@ -7,6 +7,7 @@ import com.airmouse.network.ConnectionManager
 import com.airmouse.network.UdpDiscovery
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
